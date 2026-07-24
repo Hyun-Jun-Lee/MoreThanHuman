1f33f6e200df8ca770c4bcf6d34cfb1dce051f21
Lee Hyun Jun <bhk0827@gmail.com>
2026-07-24 11:03:53 +0900

    fix : deploy option

--- deploy/scripts/certbot-renew.sh
+++ deploy/scripts/certbot-renew.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-sudo certbot renew --webroot --webroot-path=/var/www/certbot
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Run as root: sudo $0" >&2
+  exit 1
+fi
+
+certbot renew --webroot --webroot-path=/var/www/certbot
 docker compose exec nginx nginx -s reload
